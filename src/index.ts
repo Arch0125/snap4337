27f02a5734d6aea3ba324db70d93eb55be4d5a67
Archisman Das <72722967+Arch0125@users.noreply.github.com>
2023-03-11 05:46:15 +0530

    update ui

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { getAbstractAccount } from './getAbstractAccount';
 import { getBalance } from './getBalance';
 import { getHttpRpcClient } from './getHttpRpcClient';
 import { HttpRpcClient } from "@account-abstraction/sdk/dist/src/HttpRpcClient";
+import { panel, heading, text } from '@metamask/snaps-ui';
 
 
 export const getEoaAddress = async (): Promise<string> => {
@@ -34,19 +35,53 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
     case 'init_aa':
       {
         const addr = await getAddress();
-        const client = await HttpClient();
+        const eoa = await getEoaAddress();
         await snap.request({
-          method: 'snap_confirm',
-          params: [
-            {
-              prompt: 'Would you like to take the action?',
-              description: 'The action is...',
-              textAreaContent: `Very detailed information about the action...${JSON.stringify(client)}`,
-            },
-          ],
+          method: 'snap_dialog',
+          params: {
+            type: 'Confirmation',
+            content: panel([
+              heading('Account Generation'),
+              text(`Smart Contract Wallet ${addr} has been generated`),
+              text(`from EOA ${eoa}`)
+            ]),
+          },
         });
         break;
-      }
+    }
+    case 'deposit_aa':{
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const ethamount = await snap.request({
+        method: 'snap_dialog',
+        params: {
+          type: 'Prompt',
+          content: panel([
+            heading('Deposit ETH to Smart Contract Wallet'),
+            text(`Current Balance for Smart Contract Wallet ${await getAddress()} is ${await getBalance(await getAddress())} ETH`),
+            text('Enter the amount of ETH to deposit'),
+          ]),
+          placeholder: '0.01 ETH',
+        },
+      });
+      const addr = await getAddress();
+      const tx = await signer.sendTransaction({
+        to: addr,
+        value: ethers.utils.parseEther(ethamount),
+      });
+      await tx.wait();
+      await snap.request({
+        method: 'snap_dialog',
+        params: {
+          type: 'Alert',
+          content: panel([
+            heading('Deposit Complete'),
+            text(`New Balance for Smart Contract Wallet ${addr} is ${await getBalance(addr)} ETH`),
+          ]),
+        },
+      });      
+      break;
+    }
     default:
       throw new Error('Method not found.');
   }
